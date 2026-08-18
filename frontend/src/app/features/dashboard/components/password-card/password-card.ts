a6ce045fc50147ea9839bf2eb0454d48dc8d5e67
Austin-Tillotson <atillotson07@gmail.com>
2026-08-18 12:00:28 -0400

    update to display passwords in the card

--- frontend/src/app/features/dashboard/components/password-card/password-card.ts
+++ frontend/src/app/features/dashboard/components/password-card/password-card.ts
@@ -8,7 +8,7 @@ import { Card } from '../../../../shared/components/card/card';
   templateUrl: './password-card.html',
   styleUrl: './password-card.scss',
 })
-
 export class PasswordCard {
   readonly siteName = input.required<string>();
+  readonly password = input.required<string>();
 }
